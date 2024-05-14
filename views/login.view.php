93af0e4f0e20563586ab47dad54f2551d403476f
Giolianny <gioliannyt@icloud.com>
2024-05-14 22:39:36 +0200

    login finish

--- views/login.view.php
+++ views/login.view.php
@@ -6,41 +6,41 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/estilos.css">
-	<title>Registrate</title>
+	<title>Iniciar Sesion</title>
 </head>
 <body>
 	<div class="contenedor">
-		<h1 class="titulo">Registrate</h1>
+		<h1 class="titulo">Iniciar sesion</h1>
 	    <hr class= "border"></hr>
+		
 
 
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class= "formulario" name="login"> 
+        <form class="formulario" name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" > 
 
 		<div class="form-group">
 		<i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
 		</div>
 
-		<div class="form-group">
-		<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contrasena">
-		</div>
 
 		<div class="form-group">
-		<i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir Contrasena">
+		<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contrasena">
 		<i class="submit-btn fa fa-arrow-right" onclick= "login.submit()"></i>
 		</div>
 
 		<?php if(!empty($errores)): ?>
 			<div class="error">
-				<ul><?php echo $errores; ?></ul>
+				<ul>
+					<?php echo $errores; ?>
+				</ul>
 			</div>
 		<?php endif; ?>
     
         </form>
 
 		<p class= "texto-registrate">
-		Ya tienes cuenta? 
+		Aun no tienes cuenta? 
 
-		<a href="login.php">Iniciar Sesion</a>
+		<a href="registrate.php">Registrate</a>
 		</p>
 
 	</div>
